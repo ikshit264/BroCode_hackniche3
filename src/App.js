d74078b1707a084262c5f930400c96ddef4c29ee
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-03-09 04:22:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { useState } from "react";
 import { ethers } from "ethers";
 import { abi } from "./abi";
 
-const CONTRACT_ADDRESS = "0xad7C61FC480E5EEBA7886Fc62A789F9921caC9d7";
+const CONTRACT_ADDRESS = "0x358AA13c52544ECCEF6B0ADD0f801012ADAD5eE3";
 
 function App() {
   const [myContract, setMyContract] = useState(null);
