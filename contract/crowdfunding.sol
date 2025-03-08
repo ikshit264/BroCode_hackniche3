c085301506da25c4ae65ea95ed2e0964a92083c0
tusharpamnani <tusharpamnani1@gmail.com>
2025-03-08 19:42:53 +0530

    rebel-cup

--- contract/crowdfunding.sol
+++ contract/crowdfunding.sol
@@ -1,20 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-contract crowdfunding {
+contract Crowdfunding {
     enum Category {
         DESIGNANDTECH,
         FILM,
         ARTS,
         GAMES
     }
-
     enum RefundPolicy {
         REFUNDABLE,
         NONREFUNDABLE
     }
 
-    // Structure of each project in our dApp
     struct Project {
         string projectName;
         string projectDescription;
@@ -48,12 +46,6 @@ contract crowdfunding {
         Category category;
     }
 
-    // Each user funding gets recorded in Funded structure
-    struct Funded {
-        uint256 projectIndex;
-        uint256 totalAmount;
-    }
-
     // Stores all the projects
     Project[] projects;
 
@@ -63,12 +55,21 @@ contract crowdfunding {
     // Stores the list of fundings by an address
     mapping(address => Funded[]) addressFundingList;
 
-    // Checks if an index is a valid index in projects array
+    // Reputation system
+    mapping(address => uint256) reputationScore;
+
+    // Each user funding gets recorded in Funded structure
+    struct Funded {
+        uint256 projectIndex;
+        uint256 totalAmount;
+    }
+
     modifier validIndex(uint256 _index) {
         require(_index < projects.length, "Invalid Project Id");
         _;
     }
 
+    // Create a new project and updates the addressProjectsList and projects array
     // Create a new project and updates the addressProjectsList and projects array
     function createNewProject(
         string memory _name,
@@ -108,74 +109,70 @@ contract crowdfunding {
         addressProjectsList[msg.sender].push(projects.length - 1);
     }
 
+
     // Returns the project metadata of all entries in projects
-    function getAllProjectsDetail() external view returns (ProjectMetadata[] memory allProjects) {
+    function getAllProjectsDetail()
+        external
+        view
+        returns (ProjectMetadata[] memory allProjects)
+    {
         uint256 totalProjects = projects.length;
         ProjectMetadata[] memory newList = new ProjectMetadata[](totalProjects);
-        
-        // Use a helper function to create the metadata for each project
+
         for (uint256 i = 0; i < totalProjects; i++) {
-            newList[i] = createProjectMetadata(i);  // Call helper function to create metadata
+            newList[i] = createProjectMetadata(i);
         }
-        
+
         return newList;
     }
 
     // Helper function to return the metadata for a specific project index
-    function createProjectMetadata(uint256 i) internal view returns (ProjectMetadata memory) {
+    function createProjectMetadata(uint256 i)
+        internal
+        view
+        returns (ProjectMetadata memory)
+    {
         Project storage project = projects[i];
-        return ProjectMetadata(
-            project.projectName,
-            project.projectDescription,
-            project.creatorName,
-            project.cid,
-            project.fundingGoal,
-            project.amountRaised,
-            project.contributors.length,
-            project.creationTime,
-            project.duration,
-            project.category
-        );
-    }
-
-    // Takes array of indexes as parameter
-    // Returns array of metadata of project at respective indexes
-    function getProjectsDetail(uint256[] memory _indexList) external view returns (ProjectMetadata[] memory projectsList) {
-        ProjectMetadata[] memory newList = new ProjectMetadata[](_indexList.length);
-        for (uint256 index = 0; index < _indexList.length; index++) {
-            if (_indexList[index] < projects.length) {
-                uint256 i = _indexList[index];
-                newList[index] = createProjectMetadata(i);
-            } else {
-                newList[index] = ProjectMetadata(
-                    "Invalid Project",
-                    "Invalid Project",
-                    "Invalid Project",
-                    "Invalid Project",
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    Category.DESIGNANDTECH
-                );
-            }
-        }
-        return newList;
+        return
+            ProjectMetadata(
+                project.projectName,
+                project.projectDescription,
+                project.creatorName,
+                project.cid,
+                project.fundingGoal,
+                project.amountRaised,
+                project.contributors.length,
+                project.creationTime,
+                project.duration,
+                project.category
+            );
     }
 
     // Returns the project at the given index
-    function getProject(uint256 _index) external view validIndex(_index) returns (Project memory project) {
+    function getProject(uint256 _index)
+        external
+        view
+        validIndex(_index)
+        returns (Project memory project)
+    {
         return projects[_index];
     }
 
     // Returns array of indexes of projects created by creator
-    function getCreatorProjects(address creator) external view returns (uint256[] memory createdProjects) {
+    function getCreatorProjects(address creator)
+        external
+        view
+        returns (uint256[] memory createdProjects)
+    {
         return addressProjectsList[creator];
     }
 
     // Returns array of details of fundings by the contributor
-    function getUserFundings(address contributor) external view returns (Funded[] memory fundedProjects) {
+    function getUserFundings(address contributor)
+        external
+        view
+        returns (Funded[] memory fundedProjects)
+    {
         return addressFundingList[contributor];
     }
 
@@ -208,26 +205,55 @@ contract crowdfunding {
     }
 
     // Funds the projects at given index
-    function fundProject(uint256 _index) payable external validIndex(_index) {
-        require(projects[_index].creatorAddress != msg.sender, "You are the project owner");
-        require(projects[_index].duration + projects[_index].creationTime >= block.timestamp, "Project Funding Time Expired");
+    function fundProject(uint256 _index) external payable validIndex(_index) {
+        require(
+            projects[_index].creatorAddress != msg.sender,
+            "You are the project owner"
+        );
+        require(
+            projects[_index].duration + projects[_index].creationTime >=
+                block.timestamp,
+            "Project Funding Time Expired"
+        );
         addContribution(_index);
         projects[_index].amountRaised += msg.value;
+
+        reputationScore[msg.sender] += 2; // Contributors gain rep for funding
     }
 
     // Helps project creator to transfer the raised funds to his address
-    function claimFund(uint256 _index) validIndex(_index) external {
-        require(projects[_index].creatorAddress == msg.sender, "You are not Project Owner");
-        require(projects[_index].duration + projects[_index].creationTime < block.timestamp, "Project Funding Time Not Expired");
-        require(projects[_index].refundPolicy == RefundPolicy.NONREFUNDABLE 
-                    || projects[_index].amountRaised >= projects[_index].fundingGoal, "Funding goal not reached");
-        require(!projects[_index].claimedAmount, "Already claimed raised funds");
+    function claimFund(uint256 _index) external validIndex(_index) {
+        require(
+            projects[_index].creatorAddress == msg.sender,
+            "You are not Project Owner"
+        );
+        require(
+            projects[_index].duration + projects[_index].creationTime <
+                block.timestamp,
+            "Project Funding Time Not Expired"
+        );
+        require(
+            projects[_index].refundPolicy == RefundPolicy.NONREFUNDABLE ||
+                projects[_index].amountRaised >= projects[_index].fundingGoal,
+            "Funding goal not reached"
+        );
+        require(
+            !projects[_index].claimedAmount,
+            "Already claimed raised funds"
+        );
         projects[_index].claimedAmount = true;
         payable(msg.sender).transfer(projects[_index].amountRaised);
+
+        reputationScore[msg.sender] += 5; // Boost reputation for successful project completion
     }
 
     // Helper function to get the contributor index in the projects' contributor's array
-    function getContributorIndex(uint256 _index) validIndex(_index) internal view returns (int256) {
+    function getContributorIndex(uint256 _index)
+        internal
+        view
+        validIndex(_index)
+        returns (int256)
+    {
         int256 contributorIndex = -1;
         for (uint256 i = 0; i < projects[_index].contributors.length; i++) {
             if (msg.sender == projects[_index].contributors[i]) {
@@ -239,20 +265,40 @@ contract crowdfunding {
     }
 
     // Enables the contributors to claim refund when refundable project doesn't reach its goal
-    function claimRefund(uint256 _index) validIndex(_index) external {
-        require(projects[_index].duration + projects[_index].creationTime < block.timestamp, "Project Funding Time Not Expired");
-        require(projects[_index].refundPolicy == RefundPolicy.REFUNDABLE 
-                    && projects[_index].amountRaised < projects[_index].fundingGoal, "Funding goal not reached");
-        
+    function claimRefund(uint256 _index) external validIndex(_index) {
+        require(
+            projects[_index].duration + projects[_index].creationTime <
+                block.timestamp,
+            "Project Funding Time Not Expired"
+        );
+        require(
+            projects[_index].refundPolicy == RefundPolicy.REFUNDABLE &&
+                projects[_index].amountRaised < projects[_index].fundingGoal,
+            "Funding goal not reached"
+        );
+
         int256 index = getContributorIndex(_index);
         require(index != -1, "You did not contribute to this project");
-        
+
         uint256 contributorIndex = uint256(index);
-        // Fixed: Check that the refundClaimed array has this index before accessing it
-        require(contributorIndex < projects[_index].refundClaimed.length, "Refund information not available");
-        require(!projects[_index].refundClaimed[contributorIndex], "Already claimed refund amount");
-        
+        require(
+            contributorIndex < projects[_index].refundClaimed.length,
+            "Refund information not available"
+        );
+        require(
+            !projects[_index].refundClaimed[contributorIndex],
+            "Already claimed refund amount"
+        );
+
         projects[_index].refundClaimed[contributorIndex] = true;
         payable(msg.sender).transfer(projects[_index].amount[contributorIndex]);
+
+        reputationScore[msg.sender] -= 1; // Small deduction to prevent refund abuse
+        reputationScore[projects[_index].creatorAddress] -= 5; // Creator penalized for failed funding
+    }
+
+    // Get the reputation score of a user
+    function getReputation(address _user) external view returns (uint256) {
+        return reputationScore[_user];
     }
 }
